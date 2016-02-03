0df653dc1582c6fc38a6b63a8ba8032b999a0e81
Chris Park <cpark@basistech.com>
2016-02-03 11:13:57 -0500

    Improved script to handle wildcard for basename

--- rosette_apiExamples/docker/compile-and-run.sh
+++ rosette_apiExamples/docker/compile-and-run.sh
@@ -9,13 +9,23 @@ if [ $# -lt 2 ]; then
     exit 1
 fi
 
-source="../source/$1"
-executable=$(basename "$1" .cs).exe
 alt_url=""
 
 if [ ! -z "$3" ]; then
     alt_url=$3
 fi
 
-mcs $source -r:rosette_api.dll -r:System.Net.Http.dll -r:System.Web.Extensions.dll -out:$executable
-mono $executable $2 $alt_url
+executable=$(basename "$1" .cs).exe
+
+# if [ "$executable" -eq "*" ]; then
+for file in ../source/$1; do
+    source="../source/$file"
+    executable=$(basename "$file" .cs).exe
+    mcs $source -r:rosette_api.dll -r:System.Net.Http.dll -r:System.Web.Extensions.dll -out:$executable
+    mono $executable $2 $alt_url
+done
+# else
+#     mcs $source -r:rosette_api.dll -r:System.Net.Http.dll -r:System.Web.Extensions.dll -out:$executable
+#     mono $executable $2 $alt_url
+# fi
+
