d1d7ca55bdade4cf476d3777788aeb46fa571ffb
fiona <fiona@basistech.com>
2016-02-19 20:53:48 +0000

    publish csharp apidocs 0.8.0

---
+++ HTML/dir_796e59e5ab8f3d492ca45c934f39cc20.js
@@ -0,0 +1,9 @@
+var dir_796e59e5ab8f3d492ca45c934f39cc20 =
+[
+    [ "CAPI.cs", "CAPI_8cs.html", [
+      [ "CAPI", "classrosette__api_1_1CAPI.html", "classrosette__api_1_1CAPI" ],
+      [ "RosetteException", "classrosette__api_1_1RosetteException.html", "classrosette__api_1_1RosetteException" ],
+      [ "RosetteFile", "classrosette__api_1_1RosetteFile.html", "classrosette__api_1_1RosetteFile" ],
+      [ "Name", "classrosette__api_1_1Name.html", "classrosette__api_1_1Name" ]
+    ] ]
+];
---
+++ HTML/search/functions_69.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['info',['Info',['../classrosette__api_1_1CAPI.html#a5458accd3534ffc5c594a00bfd63551f',1,'rosette_api::CAPI']]]
+];
