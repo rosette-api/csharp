dc994507c65b924334d390bbd8f28e09a2c06519
fiona <fiona@basistech.com>
2016-02-19 14:26:41 -0500

    Revert "publish csharp apidocs 0.8.0"
    This reverts commit 40c918266147ba14838d488813d0d95bfc9627da.

--- search/functions_72.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['relationships',['Relationships',['../classrosette__api_1_1CAPI.html#a0c854ee70b35d09b204285e7ff2ee642',1,'rosette_api.CAPI.Relationships(string content=null, string language=null, string contentType=null, string unit=null, string contentUri=null)'],['../classrosette__api_1_1CAPI.html#a30c033bab4ace53ef9647dd20ebea335',1,'rosette_api.CAPI.Relationships(Dictionary&lt; object, object &gt; dict)'],['../classrosette__api_1_1CAPI.html#a53f7c7f45a6335c0c93db787bbb68aa0',1,'rosette_api.CAPI.Relationships(RosetteFile file)']]],
-  ['rosetteexception',['RosetteException',['../classrosette__api_1_1RosetteException.html#a065c5e007dad0541bdfa7cab0400b6b0',1,'rosette_api::RosetteException']]],
-  ['rosettefile',['RosetteFile',['../classrosette__api_1_1RosetteFile.html#a979c8781846391dc237697e09423f472',1,'rosette_api::RosetteFile']]]
-];
