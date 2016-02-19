3d8fc725a93b4db72cfc2cad5f6f9b380437a1a5
fiona <fiona@basistech.com>
2016-02-19 20:31:06 +0000

    publish csharp apidocs 0.8.0

---
+++ HTML/search/classes_6e.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['name',['Name',['../classrosette__api_1_1Name.html',1,'rosette_api']]]
+];
---
+++ HTML/search/properties_74.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['text',['text',['../classrosette__api_1_1Name.html#aea41eeac4d108ba05e5b2857f41752e5',1,'rosette_api::Name']]],
+  ['timeout',['Timeout',['../classrosette__api_1_1CAPI.html#a3e204eb9849f713e523e744e2ca5a1f1',1,'rosette_api::CAPI']]]
+];
