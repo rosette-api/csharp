fc03e1f31ee93a5a408d88f20d76230a41b45caf
fiona <fiona@basistech.com>
2016-02-25 15:27:01 +0000

    publish csharp apidocs 0.8.0

--- HTML/classrosette__api_1_1RosetteException.js
+++
@@ -1,8 +0,0 @@
-var classrosette__api_1_1RosetteException =
-[
-    [ "RosetteException", "classrosette__api_1_1RosetteException.html#a065c5e007dad0541bdfa7cab0400b6b0", null ],
-    [ "Code", "classrosette__api_1_1RosetteException.html#a2c6de2baf841c9b0453058968345b082", null ],
-    [ "File", "classrosette__api_1_1RosetteException.html#a060ff0690a35a0afbe15ecebe892a5a7", null ],
-    [ "Line", "classrosette__api_1_1RosetteException.html#a30a50a43d7edb20f3176ae4f048fa976", null ],
-    [ "RequestID", "classrosette__api_1_1RosetteException.html#a124d4746ae63df0352290ac841019163", null ]
-];
