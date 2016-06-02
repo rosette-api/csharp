dc5b977760d04543d9adc37528b28acac89fb453
fiona <fiona@basistech.com>
2016-06-02 16:26:33 -0400

    Added genre "social-media" in entities example

--- rosette_apiExamples/entities.cs
+++ rosette_apiExamples/entities.cs
@@ -35,7 +35,7 @@ namespace rosette_apiExamples
                 EntitiesCAPI.SetOption("linkEntities", false);
                 string entities_text_data = @"Bill Murray will appear in new Ghostbusters film: Dr. Peter Venkman was spotted filming a cameo in Boston this… http://dlvr.it/BnsFfS";
                 //The results of the API call will come back in the form of a Dictionary
-                RosetteResponse response = EntitiesCAPI.Entity(entities_text_data);
+                RosetteResponse response = EntitiesCAPI.Entity(entities_text_data, null, null, null, "social-media");
                 foreach (KeyValuePair<string, string> h in response.Headers) {
                     Console.WriteLine(string.Format("{0}:{1}", h.Key, h.Value));
                 }
