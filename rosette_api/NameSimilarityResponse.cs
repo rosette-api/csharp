ee7f374b93d505a73931c46d53674beca2972846
Klayton Bailey <kbailey@basistech.com>
2016-08-30 18:34:00 -0400

    name similarity response improvements, tostring, equals, hashcode, test

--- rosette_api/NameSimilarityResponse.cs
+++ rosette_api/NameSimilarityResponse.cs
@@ -18,7 +18,7 @@ namespace rosette_api
         /// <summary>
         /// The score, on a range of 0-1, of how closely the names match
         /// </summary>
-        Nullable<double> Score;
+        public decimal Score;
 
         /// <summary>
         /// The response headers returned from the API
@@ -33,9 +33,67 @@ namespace rosette_api
         {
             if (this.Content.ContainsKey(scoreKey))
             {
-                this.Score = this.Content[scoreKey] as Nullable<double>;
+                this.Score = (decimal)this.Content[scoreKey];
             }
             this.ResponseHeaders = new ResponseHeaders(this.Headers);
+        }
+
+        /// <summary>
+        /// Creates a NameSimilarityResponse from its headers
+        /// </summary>
+        /// <param name="score">The name simiarity score: 0-1</param>
+        /// <param name="responseHeaders">The response headers from the API</param>
+        /// <param name="content">The content of the response (the score) in dictionary form</param>
+        /// <param name="contentAsJSON">The content in JSON</param>
+        public NameSimilarityResponse(decimal score, Dictionary<string, string> responseHeaders, Dictionary<string, object> content, string contentAsJSON) : base(responseHeaders, content, contentAsJSON)
+        {
+            this.Score = score;
+            this.ResponseHeaders = new ResponseHeaders(responseHeaders);
+        }
+
+        /// <summary>
+        /// Equals override.
+        /// </summary>
+        /// <param name="obj">The object to compare against</param>
+        /// <returns>True if equal.</returns>
+        public override bool Equals(object obj)
+        {
+            if (obj is NameSimilarityResponse)
+            {
+                NameSimilarityResponse other = obj as NameSimilarityResponse;
+                return this.Score == other.Score && this.ResponseHeaders.Equals(other.ResponseHeaders);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// HashCode override
+        /// </summary>
+        /// <returns>The hashcode</returns>
+        public override int GetHashCode()
+        {
+            return this.Score.GetHashCode() ^ this.ResponseHeaders.GetHashCode();
+        }
+
+        /// <summary>
+        /// ToString override.  Writes this NameSimilarityResponse in JSON form
+        /// </summary>
+        /// <returns>The response in JSON form</returns>
+        public override string ToString()
+        {
+            return "{ score: " + this.Score + ", responseHeaders: " + this.ResponseHeaders.ToString() + "}";
+        }
+
+        /// <summary>
+        /// Writes the content of this response in JSON form.
+        /// </summary>
+        /// <returns>The content in JSON</returns>
+        public string ContentToString()
+        {
+            return "{\"score\":" + this.Score + "}";
         }
     }
 }
