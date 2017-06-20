34fbb05679b776ef131e779a7fad920310ff5324
Chris Park <cpark@basistech.com>
2017-06-20 11:27:11 -0700

    Updated nuspec

--- rosette_api/Properties/AssemblyInfo.cs
+++ rosette_api/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Basis Technology")]
 [assembly: AssemblyProduct("CSharp Binding")]
-[assembly: AssemblyCopyright("Copyright © Basis Technology 2016")]
+[assembly: AssemblyCopyright("Copyright © Basis Technology 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.7.0.0")]
-[assembly: AssemblyVersion("1.7.0.0")]
-[assembly: AssemblyFileVersion("1.7.0.0")]
+[assembly: AssemblyVersion("1.7.1.0")]
+[assembly: AssemblyFileVersion("1.7.1.0")]
