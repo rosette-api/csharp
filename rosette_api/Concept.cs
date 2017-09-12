c0c2281770dbb6ca2e7122ba78ad66e1288ec133
Chris Park <cpark@basistech.com>
2017-09-12 08:39:52 -0700

    RCB-527 Topics
    - New Concept.cs
    - New KeyPhrase.cs
    - New TopicsResponse.cs
    - Added Topics methods to CAPI
    - minor code cleanup

---
+++ rosette_api/Concept.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace rosette_api {
+    /// <summary>
+    /// Concepts found in a document
+    /// </summary>
+    [JsonObject(MemberSerialization.OptOut)]
+    public class Concept : IEquatable<Concept> {
+        private string phrase;
+        private double? salience;
+        private string conceptId;
+
+        /// <summary>
+        /// ctor of Concept
+        /// </summary>
+        /// <param name="phrase">phrase text of the concept</param>
+        /// <param name="salience">concept salience</param>
+        /// <param name="conceptId">ID of the concept</param>
+        public Concept(string phrase, double? salience, string conceptId) {
+            Phrase = phrase;
+            Salience = salience;
+            ConceptId = conceptId;
+        }
+
+        /// <summary>
+        /// Concept phrase
+        /// </summary>
+        [JsonProperty("phrase")]
+        public string Phrase {
+            get { return phrase; }
+            private set { phrase = value; }
+        }
+
+        /// <summary>
+        /// Concept salience
+        /// </summary>
+        [JsonProperty("salience")]
+        public double? Salience {
+            get { return salience; }
+            private set { salience = value; }
+        }
+
+        /// <summary>
+        /// ID of concept
+        /// </summary>
+        [JsonProperty("conceptId")]
+        public string ConceptId {
+            get { return conceptId; }
+            private set { conceptId = value; }
+        }
+
+        /// <summary>
+        /// Equals for Concepts
+        /// </summary>
+        /// <param name="other">Other concept to check for equality</param>
+        /// <returns>bool</returns>
+        public bool Equals(Concept other) {
+            if (other == null) {
+                return false;
+            }
+
+            return (phrase == other.Phrase && (salience == null ? true : salience == other.Salience) && conceptId == other.ConceptId);
+        }
+
+        /// <summary>
+        /// Override Equals
+        /// </summary>
+        /// <param name="obj">Object to check for equality</param>
+        /// <returns>bool</returns>
+        public override bool Equals(Object obj) {
+            if (obj == null) {
+                return false;
+            }
+
+            Concept conceptObj = obj as Concept;
+            if (conceptObj == null) {
+                return false;
+            }
+            else {
+                return Equals(conceptObj);
+            }
+        }
+
+        /// <summary>
+        /// Override GetHashCode specific to Concept
+        /// </summary>
+        /// <returns>int</returns>
+        public override int GetHashCode() {
+            return phrase.GetHashCode() ^ (salience == null ? 1 : salience.GetHashCode()) ^ conceptId.GetHashCode();
+        }
+        /// <summary>
+        /// ToString override
+        /// </summary>
+        /// <returns>This RosetteEntity in JSON form</returns>
+        public override string ToString() {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
---
+++ rosette_api/TopicsResponse.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+
+namespace rosette_api {
+    /// <summary>
+    /// A class to represent responses from the Tokenization endpoint of the Rosette API
+    /// </summary>
+    [JsonObject(MemberSerialization.OptOut)]
+    public class TopicsResponse : RosetteResponse {
+        private const string conceptsKey = "concepts";
+        private const string keyPhrasesKey = "keyphrases";
+        private const string phraseKey = "phrase";
+        private const string salienceKey = "salience";
+        private const string conceptIdKey = "conceptID";
+        /// <summary>
+        /// Gets the list of Concepts
+        /// </summary>
+        [JsonProperty(conceptsKey)]
+        public List<Concept> Concepts { get; private set; }
+        /// <summary>
+        /// Gets the list of Key Phrases
+        /// </summary>
+        [JsonProperty(keyPhrasesKey)]
+        public List<KeyPhrase> KeyPhrases { get; private set; }
+
+        /// <summary>
+        /// Creates a TopicsResponse from the given apiResults
+        /// </summary>
+        /// <param name="apiResults">The message from the API</param>
+        public TopicsResponse(HttpResponseMessage apiResults) : base(apiResults) {
+            Concepts = new List<Concept>();
+            KeyPhrases = new List<KeyPhrase>();
+
+            JArray enumerableResults = this.ContentDictionary.ContainsKey(conceptsKey) ? this.ContentDictionary[conceptsKey] as JArray : new JArray();
+            foreach (JObject result in enumerableResults) {
+                string phrase = result.Properties().Where((p) => String.Equals(p.Name, phraseKey, StringComparison.OrdinalIgnoreCase)).Any() ? result[phraseKey].ToString() : null;
+                double? salience = result.Properties().Where((p) => String.Equals(p.Name, salienceKey)).Any() ? result[salienceKey].ToObject<double?>() : null;
+                string conceptId = result.Properties().Where((p) => String.Equals(p.Name, conceptIdKey, StringComparison.OrdinalIgnoreCase)).Any() ? result[conceptIdKey].ToString() : null;
+
+                Concepts.Add(new Concept(phrase, salience, conceptId));
+            }
+            enumerableResults.Clear();
+            enumerableResults = this.ContentDictionary.ContainsKey(keyPhrasesKey) ? this.ContentDictionary[keyPhrasesKey] as JArray : new JArray();
+            foreach (JObject result in enumerableResults) {
+                string phrase = result.Properties().Where((p) => String.Equals(p.Name, phraseKey, StringComparison.OrdinalIgnoreCase)).Any() ? result[phraseKey].ToString() : null;
+                double? salience = result.Properties().Where((p) => String.Equals(p.Name, salienceKey)).Any() ? result[salienceKey].ToObject<double?>() : null;
+
+                KeyPhrases.Add(new KeyPhrase(phrase, salience));
+            }
+        }
+
+        /// <summary>
+        /// Creates a TopicsResponse from its components
+        /// </summary>
+        /// <param name="concepts">The concepts</param>
+        /// <param name="keyPhrases">The key phrases</param>
+        /// <param name="responseHeaders">The response headers returned from the API</param>
+        /// <param name="content">The content (the tokens) in dictionary form</param>
+        /// <param name="contentAsJson">The content in JSON form</param>
+        public TopicsResponse(List<Concept> concepts, List<KeyPhrase> keyPhrases, Dictionary<string, string> responseHeaders, Dictionary<string, object> content, string contentAsJson)
+            : base(responseHeaders, content, contentAsJson) {
+            Concepts = concepts;
+            KeyPhrases = keyPhrases;
+        }
+
+        /// <summary>
+        /// Equals override
+        /// </summary>
+        /// <param name="obj">The other object to compare against</param>
+        /// <returns>True if equal</returns>
+        public override bool Equals(object obj) {
+            if (obj is TopicsResponse) {
+                TopicsResponse other = obj as TopicsResponse;
+                List<bool> conditions = new List<bool>() {
+                    this.Concepts != null && other.Concepts != null ? this.Concepts.SequenceEqual(other.Concepts) : this.Concepts == other.Concepts,
+                    this.KeyPhrases != null && other.KeyPhrases != null ? this.KeyPhrases.SequenceEqual(other.KeyPhrases) : this.KeyPhrases == other.KeyPhrases,
+                    this.ResponseHeaders != null && other.ResponseHeaders != null ? this.ResponseHeaders.Equals(other.ResponseHeaders) : this.ResponseHeaders == other.ResponseHeaders,
+                    this.GetHashCode() == other.GetHashCode()
+                };
+                return conditions.All(condition => condition);
+            }
+            else {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Hashcode override
+        /// </summary>
+        /// <returns>The hashcode</returns>
+        public override int GetHashCode() {
+            int h0 = this.Concepts != null ? this.Concepts.Aggregate<Concept, int>(1, (seed, item) => seed ^ item.GetHashCode()) : 1;
+            int h1 = this.KeyPhrases != null ? this.KeyPhrases.Aggregate<KeyPhrase, int>(1, (seed, item) => seed ^ item.GetHashCode()) : 1;
+            int h2 = this.ResponseHeaders != null ? this.ResponseHeaders.GetHashCode() : 1;
+            return h0 ^ h1 ^ h2;
+        }
+    }
+}
+
