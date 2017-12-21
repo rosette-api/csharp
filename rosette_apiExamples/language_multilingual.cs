c3ed7f3fb29a7fcc39bded9a9930aad02c2db5d6
Chris Park <cpark@basistech.com>
2017-12-21 09:15:20 -0500

    WS-1272 Added Multilingual example

---
+++ rosette_apiExamples/language_multilingual.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using rosette_api;
+
+namespace rosette_apiExamples
+{
+    class language
+    {
+        /// <summary>
+        /// Example code to call Rosette API to detect possible languages for a piece of text.
+        /// Requires Nuget Package:
+        /// rosette_api
+        /// </summary>
+        static void Main(string[] args)
+        {
+            //To use the C# API, you must provide an API key
+            string apikey = "Your API key";
+            string alturl = string.Empty;
+
+            //You may set the API key via command line argument:
+            //language yourapikeyhere
+            if (args.Length != 0)
+            {
+                apikey = args[0];
+                alturl = args.Length > 1 ? args[1] : string.Empty;
+            }
+            try
+            {
+                CAPI LanguageCAPI = string.IsNullOrEmpty(alturl) ? new CAPI(apikey) : new CAPI(apikey, alturl);
+                string language_data = @"TBD";
+
+                LanguageCAPI.SetCustomHeaders("X-RosetteAPI-App", "csharp-app");
+                LanguageCAPI.SetOption("multilingual", "true");
+
+                //The results of the API call will come back in the form of a Dictionary
+                LanguageIdentificationResponse response = LanguageCAPI.Language(language_data);
+                foreach (KeyValuePair<string, string> h in response.Headers) {
+                    Console.WriteLine(string.Format("{0}:{1}", h.Key, h.Value));
+                }
+                Console.WriteLine(response.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+    }
+}
