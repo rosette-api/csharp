2f232f4f38c1c95a3f9dfee90722ca762ca8c8b5
Ian Redpath <iredpath@basistech.com>
2018-12-19 15:51:04 -0500

    RD-2427 missing greater than

--- rosette_api/RelatedTermsResponse.cs
+++ rosette_api/RelatedTermsResponse.cs
@@ -32,7 +32,7 @@ namespace rosette_api
         public RelatedTermsResponse(HttpResponseMessage apiResult)
             : base(apiResult)
         {
-            this.RelatedTerms = this.ContentDictionary.ContainsKey(RelatedTermsKey) ? this.ContentDictionary[RelatedTermsKey] as Dictionary<string, List<RelatedTerm> : new Dictionary<string, List<RelatedTerm>>();
+            this.RelatedTerms = this.ContentDictionary.ContainsKey(RelatedTermsKey) ? this.ContentDictionary[RelatedTermsKey] as Dictionary<string, List<RelatedTerm>> : new Dictionary<string, List<RelatedTerm>>();
         }
 
         /// <summary>
