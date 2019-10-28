8cb59412a9d6ec9f794e0123d9d578ef05309d8b
Katsuya Tomioka <katsuya@basistech.com>
2019-10-28 17:51:44 -0400

    DEVOPS-247: add address-similarity example

---
+++ rosette_apiExamples/address_similarity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using rosette_api;
+
+namespace rosette_apiExamples
+{
+    class address_similarity
+    {
+        /// <summary>
+        /// Example code to call Rosette API to get match score (similarity) for two addresses.
+        /// Requires Nuget Package:
+        /// rosette_api
+        /// </summary>
+        static void Main(string[] args)
+        {
+            //To use the C# API, you must provide an API key
+            string apikey = "Your API key";
+            string alturl = string.Empty;
+
+            //You may set the API key via command line argument:
+            //address_similarity yourapikeyhere
+            if (args.Length != 0)
+            {
+                apikey = args[0];
+                alturl = args.Length > 1 ? args[1] : string.Empty;
+            } 
+            try
+            {
+                CAPI cAPI = string.IsNullOrEmpty(alturl) ? new CAPI(apikey) : new CAPI(apikey, alturl);
+                //The results of the API call will come back in the form of a Dictionary
+                AddressSimilarityResponse response = cAPI.AddressSimilarity(new Address(city:"Cambridge", state:"MA"), new Address(city:"cambridge", road:"one kendall sq", state:"massachusetts"));
+                foreach (KeyValuePair<string, string> h in response.Headers) {
+                    Console.WriteLine(string.Format("{0}:{1}", h.Key, h.Value));
+                }
+                Console.WriteLine(response.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+    }
+}
