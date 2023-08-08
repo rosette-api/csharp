89f00c25fd915990edb352605f7d7924dd761729
Jonathan Ju <jju@basistech.com>
2023-08-08 12:48:48 -0400

    WS-2785: additions to event-support classes

--- rosette_api/EventMention.cs
+++ rosette_api/EventMention.cs
@@ -1,11 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
 namespace rosette_api
 {
-	public class EventMention
-	{
-		public EventMention()
+    [JsonObject(MemberSerialization.OptOut)]
+    public class EventMention : MentionOffset
+    {
+
+        /// <summary>
+        /// Gets or sets the entity's type
+        /// </summary>
+        [JsonProperty("roles")]
+        public List<EventRole> Roles { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of times this entity appeared in the input to the API
+        /// </summary>
+        [JsonProperty("polarity")]
+        public String Polarity { get; set; }
+
+        /// <summary>
+        /// Gets or sets the confidence of the extracted entity
+        /// </summary>
+        [JsonProperty("confidence")]
+        public Nullable<double> Confidence { get; set; }
+
+        /// <summary>
+        /// Gets or sets the confidence of the extracted entity
+        /// </summary>
+        [JsonProperty("negationCues")]
+        public List<NegationCues> NegationCues { get; set; }
+
+        public EventMention(int startOffsett, int endOffsett)
 		{
+
 		}
 	}
+
+    public class NegationCues
+    {
+    }
+
+    public class EventRole
+    {
+    }
 }
 
--- rosette_api/RosetteEvent.cs
+++ rosette_api/RosetteEvent.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Newtonsoft.Json;
+using static System.Collections.Specialized.BitVector32;
 
 namespace rosette_api
 {
@@ -17,22 +19,75 @@ namespace rosette_api
         /// <summary>
         /// Gets or sets the
         /// </summary>
-        [JsonProperty("normalized")]
-        public List<String> NormalizedMention { get; set; }
+        [JsonProperty("mentions")]
+        public List<EventMention> Mentions { get; set; }
 
         /// <summary>
-        /// Gets or sets 
+        /// Gets or sets the confidence of the extracted entity
         /// </summary>
-        [JsonIgnore]
-        public EntityID ID { get; set; }
-        [JsonProperty("entityId")]
-        private string EntityID { get { return ID.ToString(); } }
+        [JsonProperty("confidence")]
+        public Nullable<double> Confidence { get; set; }
 
+        /// <summary>
+        /// Creates a Rosette Event from a category label and confidence
+        /// </summary>
+        /// <param name="confidence">The confidence this was the correct event</param>
+        public RosetteEvent(String eventType, List<EventMention> mentions, Nullable<double> confidence)
+        {
+            this.EventType = eventType;
+            this.Mentions = mentions;
+            this.Confidence = confidence;
 
+        }
 
-        public RosetteEvent()
-		{
-		}
-	}
+        /// <summary>
+        /// Equals for RosetteEvent
+        /// </summary>
+        /// <param name="other">RosetteEvent</param>
+        /// <returns></returns>
+        public bool Equals(RosetteEvent other)
+        {
+            return string.Equals(EventType, other.EventType)
+                && Mentions.SequenceEqual(other.Mentions)
+                && Confidence.Equals(other.Confidence);
+        }
+
+        /// <summary>
+        /// Equals override
+        /// </summary>
+        /// <param name="obj">The object to compare against</param>
+        /// <returns>True if equal</returns>
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals(obj as RosetteEvent);
+        }
+
+        /// <summary>
+        /// HashCode override
+        /// </summary>
+        /// <returns>The hashcode</returns>
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (EventType != null ? EventType.GetHashCode() : 0);
+                hashCode = (hashCode * 401) ^ (Mentions != null ? Mentions.GetHashCode() : 0);
+                hashCode = (hashCode * 401) ^ (Confidence != null ? Confidence.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+
+        /// <summary>
+        /// ToString override
+        /// </summary>
+        /// <returns>This RosetteEvent in JSON form</returns>
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
 }
 
