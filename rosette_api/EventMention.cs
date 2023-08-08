73e43ba439a632d4bf650e4ef8733fd2fd803d60
Jonathan Ju <jju@basistech.com>
2023-08-08 13:28:53 -0400

    WS-2785: NegationCue and EventRole classes

--- rosette_api/EventMention.cs
+++ rosette_api/EventMention.cs
@@ -30,20 +30,65 @@ namespace rosette_api
         /// Gets or sets the confidence of the extracted entity
         /// </summary>
         [JsonProperty("negationCues")]
-        public List<NegationCues> NegationCues { get; set; }
+        public List<NegationCue> NegationCues { get; set; }
 
-        public EventMention(int startOffsett, int endOffsett)
+        public EventMention(List<EventRole> roles, String polarity, double? confidence, List<NegationCue> negationCues, int startOffset, int endOffset) : base (startOffset, endOffset)
 		{
-
-		}
-	}
-
-    public class NegationCues
-    {
+            this.Roles = roles;
+            this.Polarity = polarity;
+            this.Confidence = confidence;
+            this.NegationCues = negationCues;
+        }
     }
 
-    public class EventRole
+    public class EventRole : MentionOffset
     {
+        /// <summary>
+        /// Gets or sets the entity's type
+        /// </summary>
+        [JsonProperty("name")]
+        public String Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of times this entity appeared in the input to the API
+        /// </summary>
+        [JsonProperty("id")]
+        public String Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the confidence of the extracted entity
+        /// </summary>
+        [JsonProperty("dataSpan")]
+        public String DataSpan { get; set; }
+
+        /// <summary>
+        /// Gets or sets the confidence of the extracted entity
+        /// </summary>
+        [JsonProperty("confidence")]
+        public Nullable<double> Confidence { get; set; }
+
+        public EventRole(String name, String id, String dataSpan, double? confidence, int startOffset, int endOffset) : base(startOffset, endOffset)
+        {
+            this.Name = name;
+            this.Id = id;
+            this.DataSpan = dataSpan;
+            this.Confidence = confidence;
+        }
+
+    }
+
+    public class NegationCue : MentionOffset
+    {
+        /// <summary>
+        /// Gets or sets the confidence of the extracted entity
+        /// </summary>
+        [JsonProperty("dataSpan")]
+        public String DataSpan { get; set; }
+
+        public NegationCue(String name, String id, String dataSpan, double? confidence, int startOffset, int endOffset) : base(startOffset, endOffset)
+        {
+            this.DataSpan = dataSpan;
+        }
     }
 }
 
